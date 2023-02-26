4e4e5fdf13f23718b26e5b363be51b0b78c57744
anshulgoyal43 <65175818+anshulgoyal43@users.noreply.github.com>
2023-02-26 22:45:48 +0530

    Add files via upload

---
+++ time.c
@@ -0,0 +1,38 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+int main(int argc, char *argv[])
+{  
+  int wtime, rtime;
+  int pid = fork();
+
+  if(pid < 0)
+  {
+    printf(1, "Unable to fork\n");
+  }
+
+  else if(pid == 0)
+  {
+    for(int i = 0; i < argc-1; i++)
+    {
+      argv[i] = argv[i+1];
+    }
+    argv[argc-1] = 0;
+
+    exec(argv[0], argv);
+
+    // success = 0;
+    printf(1, "exec: fail\n");
+  }
+
+  else
+  {
+    // printf(1, "******time.c  %d*******\n", pid);
+    waitx(&wtime, &rtime);
+    printf(1, "rtime = %d, wtime = %d\n", rtime, wtime);
+  }
+
+  exit();
+}
